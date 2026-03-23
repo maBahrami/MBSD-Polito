0d13bdf8ebb782ecc914e683148937723d410fd3
Mammadoo <m.a.bahrami75@gmail.com>
2026-03-23 23:30:24 +0100

    using model block to reference another model, implementing model hierarchy

---
+++ Class Exercises/Exercise 2_MassSpringDamper/params_ex2.m
@@ -0,0 +1,8 @@
+clear all;
+close all;
+clc;
+
+m = 60; %  mass [kg]
+d = 36; % damping [factor Ns/m]
+k = 240; % spring coeff [N/m]
+F = 100; % force [N]
---
+++ Class Exercises/Exercise 3_PID_ModelReference/params.m
@@ -0,0 +1,14 @@
+clear all;
+close all;
+clc;
+
+% plant
+m = 60; %  mass [kg]
+d = 36; % damping [factor Ns/m]
+k = 240; % spring coeff [N/m]
+F = 100; % force [N]
+
+% controller
+Kp = 50;
+Ki = 500;
+Kd = 100;
